174da74ff12cd12ebdc9f3526b7ed25569f215d8
kavya <kavya.sudarsi@gmail.com>
2026-07-22 00:38:56 +0530

    modified booking property client and yml for deployment

--- booking-service/src/main/java/com/stayease/booking_service/client/PropertyClient.java
+++ booking-service/src/main/java/com/stayease/booking_service/client/PropertyClient.java
@@ -5,7 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 import com.stayease.booking_service.dto.PropertyDetailsResponse;
 
-@FeignClient(name = "property-service")
+@FeignClient(
+        name = "property-service",
+        url = "${property.service.url}"
+)
 public interface PropertyClient {
 
     @PutMapping("/properties/rooms/{roomId}/decrease")
